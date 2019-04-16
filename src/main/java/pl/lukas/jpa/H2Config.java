3a38ca7b06993e42d09e7a3bedd53d4122293121
LukasKodym <lukasz.kodym@gmail.com>
2019-04-16 17:06:34 +0200

    initial commit

---
+++ src/main/java/pl/lukas/jpa/H2Config.java
@@ -0,0 +1,15 @@
+package pl.lukas.jpa;
+
+import java.sql.Connection;
+
+public class H2Config {
+
+    private Connection getDBConection(){
+
+        Connection connection = null;
+
+        return connection;
+
+    }
+
+}
