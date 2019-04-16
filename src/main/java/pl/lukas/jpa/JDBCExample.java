4eec1d224f6ff69e65c2b6b22ecc44ab68927636
LukasKodym <lukasz.kodym@gmail.com>
2019-04-16 17:39:51 +0200

    JDBC students added

--- src/main/java/pl/lukas/jpa/JDBCExample.java
+++ src/main/java/pl/lukas/jpa/JDBCExample.java
@@ -1,10 +1,25 @@
 package pl.lukas.jpa;
 
+import pl.lukas.jpa.domain.Student;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JDBCExample {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        createTableForStudent();
+        Student student1 = new Student(1, "Paweł");
+        Student student2 = new Student(2, "Marek");
+
+        insertStudent(student1);
+        insertStudent(student2);
+
+
+    }
 
     private static void createTableForStudent() throws SQLException, ClassNotFoundException {
         Connection connection = H2Config.getDBConnection();
@@ -16,5 +31,22 @@ public class JDBCExample {
         statement.execute(createTable);
 
         connection.commit();
+    }
+
+    private static void insertStudent(Student student) throws SQLException, ClassNotFoundException {
+        Connection connection = H2Config.getDBConnection();
+
+        Statement statement = connection.createStatement();
+
+        String insert = "INSERT INTO STUDENT VALUES(" + student.getId() + ",\'" + student.getName() + "\')";
+
+        statement.execute(insert);
+
+        connection.commit();
+
+    }
+
+    private static List<Student> getStudents(){
+        List<Student> students = new ArrayList<Student>();
     }
 }
