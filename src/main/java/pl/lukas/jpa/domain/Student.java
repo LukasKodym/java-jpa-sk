5177a33a7b8cfac08dea3329ed9f51b0834d79d5
LukasKodym <lukasz.kodym@gmail.com>
2019-04-17 17:33:52 +0200

    annotation @Entity end 3.5

--- src/main/java/pl/lukas/jpa/domain/Student.java
+++ src/main/java/pl/lukas/jpa/domain/Student.java
@@ -1,9 +1,20 @@
 package pl.lukas.jpa.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Student {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
+
+        private String telephone;
+
+
+    public Student() {
+    }
 
     public Student(int id, String name) {
         this.id = id;
